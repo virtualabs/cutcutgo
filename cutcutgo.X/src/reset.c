74268e6b9fa80703dbf720fce8d961a5bf665e45
virtualabs <virtualabs@gmail.com>
2023-09-29 01:16:52 +0200

    Added specific code to handle software reset.

---
+++ cutcutgo.X/src/reset.c
@@ -0,0 +1,19 @@
+#include "reset.h"
+
+void reset_soft(void)
+{
+    /* Software reset */
+    SYSKEY = 0x00000000; //write invalid key to force lock
+    SYSKEY = 0xAA996655; //write key1 to SYSKEY
+    SYSKEY = 0x556699AA; //write key2 to SYSKEY
+
+    /* set SWRST bit to arm reset */
+    RSWRSTSET = 1;
+
+    /* read RSWRST register to trigger reset */
+    unsigned int dummy;
+    dummy = RSWRST;
+
+    /* prevent any unwanted code execution until reset occurs*/
+    while(1);
+}
---
+++ cutcutgo.X/src/reset.h
@@ -0,0 +1,26 @@
+/* 
+ * File:   reset.h
+ * Author: virtualabs
+ *
+ * Created on 28 septembre 2023, 23:58
+ */
+
+#ifndef RESET_H
+#define	RESET_H
+
+#include "configuration.h"
+#include "definitions.h"
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+void reset_soft(void);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* RESET_H */
+
