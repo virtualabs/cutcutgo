59a4e27011eadad4357d5f4535297047d22c223c
virtualabs <virtualabs@gmail.com>
2023-11-30 23:18:19 +0100

    Enabled per-axis homing. This is required for startup and shutdown procedures.

--- cutcutgo.X/src/grbl/grbl/config.h
+++ cutcutgo.X/src/grbl/grbl/config.h
@@ -124,7 +124,7 @@
 // cycle is still invoked by the $H command. This is disabled by default. It's here only to address
 // users that need to switch between a two-axis and three-axis machine. This is actually very rare.
 // If you have a two-axis machine, DON'T USE THIS. Instead, just alter the homing cycle for two-axes.
-// #define HOMING_SINGLE_AXIS_COMMANDS // Default disabled. Uncomment to enable.
+#define HOMING_SINGLE_AXIS_COMMANDS // Default disabled. Uncomment to enable.
 
 // After homing, Grbl will set by default the entire machine space into negative space, as is typical
 // for professional CNC machines, regardless of where the limit switches are located. Uncomment this
