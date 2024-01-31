9e111ca31c2d730f58bba913120624e2cf0c4baa
virtualabs <virtualabs@gmail.com>
2024-01-31 01:07:37 +0100

    Add custom timer API.

--- cutcutgo.X/src/hal/timer.c
+++ cutcutgo.X/src/hal/timer.c
@@ -7,12 +7,27 @@
 
 volatile uint32_t g_ticks_count;
 
+/* Timers. */
+static timerslot_t g_timer_pool[TIMER_POOL_SIZE];
+
 /**
  * @brief   Initialize system timers
  */
 
 void timer_init(void)
 {
+    int i;
+    
+    /* Initialize our timers pool. */
+    for (i=0; i<TIMER_POOL_SIZE; i++)
+    {
+        g_timer_pool[i].p_callback = NULL;
+        g_timer_pool[i].p_data = NULL;
+        g_timer_pool[i].interval = 0;
+        g_timer_pool[i].state = TIMER_FREE;
+        g_timer_pool[i].stop_ts = 0;
+    }
+    
     /* Setup Timer1 (main system timer) */
     T1CON   = 0x0;      // Disable timer 1 when setting it up
     TMR1    = 0;        // Set timer 1 counter to 0
@@ -32,14 +47,37 @@ void timer_init(void)
     T1CONbits.TON = 1;
 }
 
+void timer_trigger(void)
+{
+    int i;
+    
+    for (i=0; i<TIMER_POOL_SIZE; i++)
+    {
+        if (g_timer_pool[i].state == TIMER_RUNNING)
+        {
+            g_timer_pool[i].stop_ts--;
+            if (g_timer_pool[i].stop_ts == 0)
+            {
+                /* Timer is automatically stopped. */
+                g_timer_pool[i].state = TIMER_STOPPED;
+                
+                /* Trigger the associated callback. */
+                g_timer_pool[i].p_callback(g_timer_pool[i].p_data);
+            }
+        }
+    }
+}
 
 void __ISR(_TIMER_1_VECTOR, ipl1SOFT) Timer1Handler (void) {
 
     /* Increment our counter. */
     g_ticks_count++;
     
+    /* Check our timer slots. */
+    timer_trigger();
+    
     /* Run our watchdog. */
-    watchdog_callback();
+    //watchdog_callback();
     
     /* Blink LED if required. */
     led_handle_blink();
@@ -69,4 +107,111 @@ void _delay_ms(uint32_t ms)
 uint32_t timer_get_ms(void)
 {
     return g_ticks_count;
+}
+
+
+
+int timer_create_timer(uint32_t interval_ms, timerslot_cb_t *p_callback, void *p_data)
+{
+    int i;
+    
+    /* Search a free timer in our pool. */
+    for (i=0; i<TIMER_POOL_SIZE; i++)
+    {
+        if (g_timer_pool[i].state == TIMER_FREE)
+        {
+            g_timer_pool[i].p_callback = p_callback;
+            g_timer_pool[i].p_data = p_data;
+            g_timer_pool[i].interval = interval_ms;
+            g_timer_pool[i].state = TIMER_STOPPED;
+            return i;
+        }
+    }
+    
+    /* No free timer available. */
+    return -1;
+}
+
+int timer_set_interval(int timer_id, uint32_t interval)
+{
+    if ((timer_id >=0) && (timer_id < TIMER_POOL_SIZE))
+    {
+        g_timer_pool[timer_id].interval = interval;
+        
+        /* Success. */
+        return 0;
+    }
+    else
+    {
+        /* Error. */
+        return -1;
+    }
+}
+
+int timer_start_timer(int timer_id)
+{
+    if ((timer_id >=0) && (timer_id < TIMER_POOL_SIZE))
+    {
+        if (g_timer_pool[timer_id].state == TIMER_STOPPED)
+        {
+            /* Compute the stop timestamp in ms. */
+            g_timer_pool[timer_id].stop_ts = g_timer_pool[timer_id].interval;
+            
+            /* Start timer. */
+            g_timer_pool[timer_id].state = TIMER_RUNNING;
+            
+            /* Success. */
+            return 0;
+        }
+        else
+        {
+            /* Error, timer is already started. */
+            return -1;
+        }
+    }
+    else
+    {
+        /* Error. */
+        return -1;   
+    }
+}
+
+int timer_stop_timer(int timer_id)
+{
+    if ((timer_id >=0) && (timer_id < TIMER_POOL_SIZE))
+    {
+        if (g_timer_pool[timer_id].state == TIMER_RUNNING)
+        {
+            g_timer_pool[timer_id].state = TIMER_STOPPED;
+
+            /* Success. */
+            return 0;
+        }
+        else
+        {
+            /* Error. */
+            return -1;
+        }
+    }
+    else
+    {
+        /* Error. */
+        return -1;
+    }
+}
+
+int timer_destroy_timer(int timer_id)
+{
+    if ((timer_id >=0) && (timer_id < TIMER_POOL_SIZE))
+    {
+        g_timer_pool[timer_id].state = TIMER_FREE;
+
+        /* Success. */
+        return 0;
+    }
+    else
+    {
+        /* Error. */
+        return -1;
+    }   
 }
--- cutcutgo.X/src/hal/timer.h
+++ cutcutgo.X/src/hal/timer.h
@@ -7,13 +7,36 @@
 
 #include <stdint.h>
 
+#define TIMER_POOL_SIZE 10
+
+typedef void (timerslot_cb_t)(void *p_data);
+
+typedef enum {
+    TIMER_FREE,
+    TIMER_STOPPED,
+    TIMER_RUNNING
+} timerslot_state_t;
+
+typedef struct {
+    uint32_t interval;
+    uint32_t stop_ts;
+    void *p_data;
+    timerslot_cb_t *p_callback;
+    timerslot_state_t state;
+} timerslot_t;
+
 /* Initialize system timers. */
 void timer_init(void);
 void timer_start(void);
 uint32_t timer_get_ms(void);
-
 void _delay_ms(uint32_t ms);
 
+/* Timer creation, start, stop, destruction. */
+int timer_create_timer(uint32_t interval_ms, timerslot_cb_t *p_callback, void *p_data);
+int timer_set_interval(int timer_id, uint32_t interval);
+int timer_start_timer(int timer_id);
+int timer_stop_timer(int timer_id);
+int timer_destroy_timer(int timer_id);
 
 
 #endif /* __INC_HAL_TIMER_H */
